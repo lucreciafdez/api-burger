574c74fd23e8a6b06889cd271957812b8aec6217
lucreciafdez <lfernandez4@uc.cl>
2020-04-27 17:32:43 -0400

    cambio

--- app/models/ingrediente.rb
+++ app/models/ingrediente.rb
@@ -2,6 +2,5 @@ class Ingrediente < ApplicationRecord
   has_and_belongs_to_many :hamburguesas
   validates :nombre, presence: true
   validates :descripcion, presence: true
-  @ingrediente.path = "burger-api-taller.herokuapp.com/ingrediente/#{@ingrediente.id}"
 
 end
